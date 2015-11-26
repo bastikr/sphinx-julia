b16de37744d3a07df6a6d33cb2514a6a6cb35dae
Sebastian Kraemer <basti.kr@gmail.com>
2015-11-26 21:13:09 +0100

    Update config.py file for documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Sphinx-Julia'
-copyright = '2015, Sebastian Krämer'
-author = 'Sebastian Krämer'
+copyright = u'2015, Sebastian Krämer'
+author = u'Sebastian Krämer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -112,7 +112,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
