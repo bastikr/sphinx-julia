2a6a3c7a06bcb16477263e537985fc325d27401b
Sebastian Kraemer <basti.kr@gmail.com>
2015-11-27 16:33:55 +0100

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sphinx-Julia',
+      version='0.1',
+      description='Sphinx extensions for Julia',
+      author='Sebastian Kraemer',
+      author_email='basti.kr@gmail.com',
+      url='https://github.com/bastikr/sphinx-julia',
+      license='MIT',
+      packages=['sphinxjulia'],
+      package_data={'sphinxjulia': ['parsetools/*.jl']},
+     )
