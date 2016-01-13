f5b69ee217126c1b1d2e5467321188daac1184a3
Sebastian Kraemer <basti.kr@gmail.com>
2016-01-13 10:04:11 +0100

    Update setup.py for version 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='Sphinx-Julia',
-      version='0.1',
+      version='0.2',
       description='Sphinx extensions for Julia',
       author='Sebastian Kraemer',
       author_email='basti.kr@gmail.com',
       url='https://github.com/bastikr/sphinx-julia',
       license='MIT',
       packages=['sphinxjulia'],
-      package_data={'sphinxjulia': ['parsetools/*.jl']},
+      package_data={'sphinxjulia': ['parsetools/*/*.jl']},
      )
